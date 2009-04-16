16624512e4e52162a17ee7ef891da92143109ecd
Ian Rogers <ian.rogers@manchester.ac.uk>
2009-04-16 01:23:01 +0100

    Don't use MMTk managed stacks in Windows.

--- tools/bootloader/sysThread_Intel.c
+++ tools/bootloader/sysThread_Intel.c
@@ -57,13 +57,7 @@ void bootThread (void *_ip, void *_tr, void *_sp, void UNUSED *_jtoc)
   __asm{
       mov eax, _ip
       mov esi, _tr
-      mov saved_ebp, ebp
-      mov ebp, esp
-      mov esp, _sp
-      push ebp
       call eax
-      pop esp
-      mov ebp, saved_ebp
   }
 #endif
 }
