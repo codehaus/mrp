8d40e38a87d5d57f433115f8b140823e22797c4c
Ian Rogers <ian.rogers@manchester.ac.uk>
2009-04-28 15:34:10 +0100

    Support for new condition operands in LIR2MIR conversion.

--- rvm/src/org/jikesrvm/compilers/opt/ir/operand/ia32/IA32ConditionOperand.java
+++ rvm/src/org/jikesrvm/compilers/opt/ir/operand/ia32/IA32ConditionOperand.java
@@ -49,56 +49,24 @@ public final class IA32ConditionOperand extends Operand implements AssemblerCons
    */
   public IA32ConditionOperand flipCode() {
     switch (value) {
-      case O:
-        value = NO;
-        break;
-      case NO:
-        value = O;
-        break;
-      case LLT:
-        value = LGE;
-        break;
-      case LGE:
-        value = LLT;
-        break;
-      case EQ:
-        value = NE;
-        break;
-      case NE:
-        value = EQ;
-        break;
-      case LLE:
-        value = LGT;
-        break;
-      case LGT:
-        value = LLE;
-        break;
-      case S:
-        value = NS;
-        break;
-      case NS:
-        value = S;
-        break;
-      case PE:
-        value = PO;
-        break;
-      case PO:
-        value = PE;
-        break;
-      case LT:
-        value = GE;
-        break;
-      case GE:
-        value = LT;
-        break;
-      case LE:
-        value = GT;
-        break;
-      case GT:
-        value = LE;
-        break;
-      default:
-        OptimizingCompilerException.UNREACHABLE();
+    case O:   value =  NO; break;
+    case NO:  value =   O; break;
+    case LLT: value = LGE; break;
+    case LGE: value = LLT; break;
+    case EQ:  value =  NE; break;
+    case NE:  value =  EQ; break;
+    case LLE: value = LGT; break;
+    case LGT: value = LLE; break;
+    case S:   value =  NS; break;
+    case NS:  value =   S; break;
+    case PE:  value =  PO; break;
+    case PO:  value =  PE; break;
+    case LT:  value =  GE; break;
+    case GE:  value =  LT; break;
+    case LE:  value =  GT; break;
+    case GT:  value =  LE; break;
+    default:
+      OptimizingCompilerException.UNREACHABLE();
     }
     return this;
   }
@@ -109,32 +77,16 @@ public final class IA32ConditionOperand extends Operand implements AssemblerCons
    */
   public IA32ConditionOperand flipOperands() {
     switch (value) {
-      case LLT:
-        value = LGT;
-        break;
-      case LGE:
-        value = LLE;
-        break;
-      case LLE:
-        value = LGE;
-        break;
-      case LGT:
-        value = LLT;
-        break;
-      case LT:
-        value = GT;
-        break;
-      case GE:
-        value = LE;
-        break;
-      case LE:
-        value = GE;
-        break;
-      case GT:
-        value = LT;
-        break;
-      default:
-        OptimizingCompilerException.TODO();
+    case LLT: value = LGT; break;
+    case LGE: value = LLE; break;
+    case LLE: value = LGE; break;
+    case LGT: value = LLT; break;
+    case LT:  value =  GT; break;
+    case GE:  value =  LE; break;
+    case LE:  value =  GE; break;
+    case GT:  value =  LT; break;
+    default:
+      OptimizingCompilerException.TODO();
     }
     return this;
   }
@@ -209,50 +161,72 @@ public final class IA32ConditionOperand extends Operand implements AssemblerCons
 
   // translate from ConditionOperand: used during LIR => MIR translation
   private void translate(ConditionOperand c) {
-    switch (c.value) {
-      case ConditionOperand.EQUAL:
-        value = EQ;
-        break;
-      case ConditionOperand.NOT_EQUAL:
-        value = NE;
-        break;
-      case ConditionOperand.LESS:
-        value = LT;
-        break;
-      case ConditionOperand.LESS_EQUAL:
-        value = LE;
-        break;
-      case ConditionOperand.GREATER:
-        value = GT;
-        break;
-      case ConditionOperand.GREATER_EQUAL:
-        value = GE;
-        break;
-      case ConditionOperand.HIGHER:
-        value = LGT;
-        break;
-      case ConditionOperand.LOWER:
-        value = LLT;
-        break;
-      case ConditionOperand.HIGHER_EQUAL:
-        value = LGE;
-        break;
-      case ConditionOperand.LOWER_EQUAL:
-        value = LLE;
-        break;
-      case ConditionOperand.CMPL_EQUAL:
-      case ConditionOperand.CMPL_GREATER:
-      case ConditionOperand.CMPG_LESS:
-      case ConditionOperand.CMPL_GREATER_EQUAL:
-      case ConditionOperand.CMPG_LESS_EQUAL:
-      case ConditionOperand.CMPL_NOT_EQUAL:
-      case ConditionOperand.CMPL_LESS:
-      case ConditionOperand.CMPG_GREATER_EQUAL:
-      case ConditionOperand.CMPG_GREATER:
-      case ConditionOperand.CMPL_LESS_EQUAL:
-        throw new Error("IA32ConditionOperand.translate: Complex operand can't be directly translated " + c);
-      default:
-        OptimizingCompilerException.UNREACHABLE();
+    switch(c.value) {
+    case ConditionOperand.EQUAL:
+      value =  EQ;
+      break;
+    case ConditionOperand.NOT_EQUAL:
+      value =  NE;
+      break;
+    case ConditionOperand.LESS:
+      value =  LT;
+      break;
+    case ConditionOperand.LESS_EQUAL:
+      value =  LE;
+      break;
+    case ConditionOperand.GREATER:
+      value =  GT;
+      break;
+    case ConditionOperand.GREATER_EQUAL:
+      value =  GE;
+      break;
+    case ConditionOperand.HIGHER:
+      value = LGT;
+      break;
+    case ConditionOperand.LOWER:
+    case ConditionOperand.CARRY_FROM_ADD:
+    case ConditionOperand.BORROW_FROM_SUB:
+    case ConditionOperand.BORROW_FROM_RSUB:
+    case ConditionOperand.BIT_TEST:
+    case ConditionOperand.RBIT_TEST:
+      value = LLT;
+      break;
+    case ConditionOperand.HIGHER_EQUAL:
+    case ConditionOperand.NO_CARRY_FROM_ADD:
+    case ConditionOperand.NO_BORROW_FROM_SUB:
+    case ConditionOperand.NO_BORROW_FROM_RSUB:
+    case ConditionOperand.NO_BIT_TEST:
+    case ConditionOperand.NO_RBIT_TEST:
+      value = LGE;
+      break;
+    case ConditionOperand.LOWER_EQUAL:
+      value = LLE;
+      break;
+    case ConditionOperand.OVERFLOW_FROM_ADD:
+    case ConditionOperand.OVERFLOW_FROM_SUB:
+    case ConditionOperand.OVERFLOW_FROM_RSUB:
+    case ConditionOperand.OVERFLOW_FROM_MUL:
+      value =   O;
+      break;
+    case ConditionOperand.NO_OVERFLOW_FROM_ADD:
+    case ConditionOperand.NO_OVERFLOW_FROM_SUB:
+    case ConditionOperand.NO_OVERFLOW_FROM_RSUB:
+    case ConditionOperand.NO_OVERFLOW_FROM_MUL:
+      value =  NO;
+      break;
+    case ConditionOperand.CMPL_EQUAL:
+    case ConditionOperand.CMPL_GREATER:
+    case ConditionOperand.CMPG_LESS:
+    case ConditionOperand.CMPL_GREATER_EQUAL:
+    case ConditionOperand.CMPG_LESS_EQUAL:
+    case ConditionOperand.CMPL_NOT_EQUAL:
+    case ConditionOperand.CMPL_LESS:
+    case ConditionOperand.CMPG_GREATER_EQUAL:
+    case ConditionOperand.CMPG_GREATER:
+    case ConditionOperand.CMPL_LESS_EQUAL:
+      throw new Error("IA32ConditionOperand.translate: Complex operand can't be directly translated " + c);
+    default:
+      OptimizingCompilerException.UNREACHABLE();
     }
   }
 
