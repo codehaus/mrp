b678878932c3391575fa9cc8a25b37c910721912
Ian Rogers <ian.rogers@manchester.ac.uk>
2009-04-28 22:25:09 +0100

    Bring up-to-date with RVM r15675 (CPL to EPL transition, BasicBlock bug fix), not updating to current HEAD which is broken on Harmony.

--- rvm/src/org/jikesrvm/compilers/opt/escape/SimpleEscape.java
+++ rvm/src/org/jikesrvm/compilers/opt/escape/SimpleEscape.java
@@ -1,11 +1,11 @@
 /*
  *  This file is part of the Jikes RVM project (http://jikesrvm.org).
  *
- *  This file is licensed to You under the Common Public License (CPL);
+ *  This file is licensed to You under the Eclipse Public License (EPL);
  *  You may not use this file except in compliance with the License. You
  *  may obtain a copy of the License at
  *
- *      http://www.opensource.org/licenses/cpl1.0.php
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
  *
  *  See the COPYRIGHT.txt file distributed with this work for information
  *  regarding copyright ownership.
@@ -485,6 +485,10 @@ class SimpleEscape extends CompilerPhase {
         }
         // pure methods don't let object escape
         if (mop.getTarget().isPure()) {
+          return false;
+        }
+        // Assume non-annotated native methods let object escape
+        if (mop.getTarget().isNative()) {
           return false;
         }
         // try to get a method summary for the called method
