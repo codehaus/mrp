c4e305e7aa1255faa5318690106f7dc829075643
Ian Rogers <ian.rogers@manchester.ac.uk>
2009-05-28 01:14:02 +0100

    Reduce visibility from public to package where possible.

--- rvm/src/org/jikesrvm/compilers/baseline/BasicBlock.java
+++ rvm/src/org/jikesrvm/compilers/baseline/BasicBlock.java
@@ -25,10 +25,10 @@ final class BasicBlock {
 
   // ------------------- Static Class Fields -----------------
 
-  public static final int NOTBLOCK = 0;
-  public static final int EXITBLOCK = 1;
-  public static final int STARTPREDSIZE = 4;
-  public static final int STARTBBNUMBER = 2;
+  static final int NOTBLOCK = 0;
+  static final int EXITBLOCK = 1;
+  static final int STARTPREDSIZE = 4;
+  static final int STARTBBNUMBER = 2;
 
   static final byte JSRENTRY = 1;
   static final byte JSREXIT = 2;
@@ -79,7 +79,7 @@ final class BasicBlock {
   // ------------------ Static Methods -------------------------
 
   /** transfer predecessor blocks from one block to another */
-  public static void transferPredecessors(BasicBlock fromBB,
+  static void transferPredecessors(BasicBlock fromBB,
       BasicBlock toBB) {
     toBB.predcount = fromBB.predcount;
     toBB.pred1 = fromBB.pred1;
@@ -104,51 +104,51 @@ final class BasicBlock {
     state |= stateval;
   }
 
-  public int getStart() {
+  int getStart() {
     return start;
   }
 
-  public int getEnd() {
+  int getEnd() {
     return end;
   }
 
-  public int getBlockNumber() {
+  int getBlockNumber() {
     return blockNumber;
   }
 
-  public byte getState() {
+  byte getState() {
     return state;
   }
 
-  public boolean isJSRExit() {
+  boolean isJSRExit() {
     return ((state & JSREXIT) == JSREXIT);
   }
 
-  public boolean isJSREntry() {
+  boolean isJSREntry() {
     return ((state & JSRENTRY) == JSRENTRY);
   }
 
-  public boolean isInJSR() {
+  boolean isInJSR() {
     return ((state & INJSR) == INJSR);
   }
 
-  public boolean isMethodEntry() {
+  boolean isMethodEntry() {
     return ((state & METHODENTRY) == METHODENTRY);
   }
 
-  public boolean isTryStart() {
+  boolean isTryStart() {
     return ((state & TRYSTART) == TRYSTART);
   }
 
-  public boolean isTryBlock() {
+  boolean isTryBlock() {
     return ((state & TRYBLOCK) == TRYBLOCK);
   }
 
-  public boolean isTryHandlerStart() {
+  boolean isTryHandlerStart() {
     return ((state & TRYHANDLERSTART) == TRYHANDLERSTART);
   }
 
-  public void addPredecessor(BasicBlock predbb) {
+  void addPredecessor(BasicBlock predbb) {
     predcount++;
     if (predcount == 1) {
       pred1 = (short) predbb.getBlockNumber();
@@ -176,7 +176,7 @@ final class BasicBlock {
    * This method first checks if a block is already on the predecessor list.
    * Used with try blocks being added to their catch block as predecessors.
    */
-  public void addUniquePredecessor(BasicBlock predbb) {
+  void addUniquePredecessor(BasicBlock predbb) {
     boolean dupFound = false, checkMade = false;
     short predbbNum = (short) predbb.getBlockNumber();
 
@@ -234,7 +234,7 @@ final class BasicBlock {
     }
   }
 
-  public int[] getPredecessors() {
+  int[] getPredecessors() {
     int[] preds;
     preds = new int[predcount];
     if (predcount >= 1) {
